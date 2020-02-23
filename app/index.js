a4711ae1f569f114dae8a724ac1d13d1e22651b3
Wojtaszko, Pawel <pwojtaszko@microstrategy.com>
2020-02-23 22:58:17 +0100

    Added weather view with all details. Optimized images

--- app/index.js
+++ app/index.js
@@ -5,23 +5,16 @@ import { HeartRateSensor } from "heart-rate";
 import { BodyPresenceSensor } from "body-presence";
 import { display } from "display";
 import { today } from 'user-activity';
+import { preferences } from "user-settings";
 
+// global variables
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
-
-const weatherInterval = null;
-
-// Update the clock every second
-clock.granularity = "seconds";
-
-const hourHand = document.getElementById("hours");
-const minHand = document.getElementById("mins");
-const secHand = document.getElementById("secs");
-const heartRateText = document.getElementById("heartratetext");
+const daysNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const dateText = document.getElementById("dateText");
 const img = document.getElementById("weatherIcon");
-const cityname = document.getElementById("cityname");
+const cityname = document.getElementById("cityname").getElementById('text');
 const degrees = document.getElementById("degrees");
 const reloadWeatherButton = document.getElementById("weatherRefreshButton");
 const weatherButton = document.getElementById("weatherButton");
@@ -31,29 +24,66 @@ const toastElement = document.getElementById("toastUse");
 const toastText =   document.getElementById("toastText");
 const goToClockButton = document.getElementById("goToClockButton");
 const weatherView = document.getElementById("weatherView");
+const hourHand = document.getElementById("hours");
+const minHand = document.getElementById("mins");
+const secHand = document.getElementById("secs");
+const detailsCityName = document.getElementById("detailsCityName");
+
+let hrm = null; //heart rate sensor data
+let bodyPresence = null; //body presence sensor data
 
 let toastTimeout = null;
 let container = document.getElementById("container");
 
-// Returns an angle (0-360) for the current hour in the day, including minutes
-function hoursToAngle(hours, minutes) {
+const hoursToAngle = (hours, minutes) => {
   let hourAngle = (360 / 12) * hours;
   let minAngle = (360 / 12 / 60) * minutes;
   return hourAngle + minAngle;
 }
 
-// Returns an angle (0-360) for minutes
-function minutesToAngle(minutes) {
+const minutesToAngle = (minutes) => {
   return (360 / 60) * minutes;
 }
 
-// Returns an angle (0-360) for seconds
-function secondsToAngle(seconds) {
+const secondsToAngle = (seconds) => {
   return (360 / 60) * seconds;
 }
 
-// Rotate the hands every tick
-function updateClock() {
+const setAodListener = () => {
+  if (display.aodAvailable) {
+    display.aodAllowed = true;
+
+    const weatherSection = document.getElementById("weather");
+    const heartRateSection = document.getElementById("heartRate");
+    const stepsSection = document.getElementById("steps");
+    const minutesLayer = document.getElementById("minutesLayer");
+
+    display.addEventListener("change", () => {
+      if (!display.aodActive && display.on) {
+        container.value = 0;
+        clock.granularity = "seconds";
+        secHand.style.display = "inline";
+        weatherSection.style.display = "inline";
+        heartRateSection.style.display = "inline";
+        stepsSection.style.display = "inline";
+        minutesLayer.style.display = "inline";
+        hrm.start();
+        bodyPresence.start();
+      } else {
+        clock.granularity = "minutes";
+        secHand.style.display = "none";
+        weatherSection.style.display = "none";
+        heartRateSection.style.display = "none";
+        stepsSection.style.display = "none";
+        minutesLayer.style.display = "none";
+        hrm.stop();
+        bodyPresence.stop();
+      }
+    });
+  }
+};
+
+const hanldeClockTick = () => {
   let todayDate = new Date();
   let hours = todayDate.getHours() % 12;
   let mins = todayDate.getMinutes();
@@ -67,8 +97,7 @@ function updateClock() {
   dateText.text = todayDate.getDate() + " " + monthNames[todayDate.getMonth()].substring(0, 3);
 }
 
-// Request weather data from the companion
-function fetchWeather() {
+const fetchTodayWeather = () => {
   weatherButtonIcon.style.display = "inline";
   weatherButtonIcon.animate('enable');
   cityname.text = '';
@@ -76,7 +105,17 @@ function fetchWeather() {
   degrees.text = '';
   if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
     messaging.peerSocket.send({
-      command: 'weather'
+      command: 'todayWeather'
+    });
+  } else {
+    displayToast("Failed loading weather. Open Fitbit app on your phone.");
+  }
+}
+
+const fetch5daysWeather = () => {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send({
+      command: 'forecastWeather'
     });
   } else {
     displayToast("Failed loading weather. Open Fitbit app on your phone.");
@@ -94,133 +133,132 @@ const displayToast = (message) => {
   }, 3000);
 }  
 
-// Update the clock every tick event
-clock.ontick = () => updateClock();
+const setWeatherListener = () => {
+  messaging.peerSocket.onopen = () => {
+    fetchTodayWeather();
+  }
 
-// Listen for the onopen event
-messaging.peerSocket.onopen = function() {
-  fetchWeather();
+  messaging.peerSocket.onerror = () => {
+    displayToast("Connection error: waiting for socket to open");
+  }
+
+  const weatherInterval = null;
+  messaging.peerSocket.onmessage = (evt) => {
+    const data = evt.data;
+    switch (data.command) {
+      case 'todayWeather':
+        if (data.enabled === 'true')  {
+          weatherView.style.display = "inline";
+          reloadWeatherButton.style.display = "inline";
+          weatherButton.style.display = "inline";
+          weatherButtonIcon.style.display = "inline";
+          const updateMinutes = data.updateEveryMinutes ? data.updateEveryMinutes : 30;  
+          if (data.temperature) {
+            const el = data.weatherElement;
+            cityname.text = data.cityName;
+            document.getElementById('customTextarea');
+            img.href = "weatherimages/"+el.icon+".png";
+            degrees.text = Math.round(data.temperature) + "°";
+            weatherButtonIcon.style.display = "none";
+            if(weatherInterval !== null) {
+              clearInterval(weatherInterval);
+            }
+            weatherInterval = setInterval(fetchTodayWeather, Number(updateMinutes) * 1000 * 60);
+          }
+          if (data.error !== null) {
+            displayToast(data.error);
+          }
+        } else {
+          weatherView.style.display = "none";
+          reloadWeatherButton.style.display = "none";
+          weatherButton.style.display = "none";
+          weatherButtonIcon.style.display = "none";
+          cityname.text = '';
+          img.href = '';
+          degrees.text = '';
+        }
+        break;
+      case 'forecastWeather':
+        if (data.enabled === 'true')  {
+          detailsCityName.text = data.cityName;
+        }
+        if (data.svgElement) {
+          const messages = data.weatherDayMessage;
+          
+          const svgElement = document.getElementById("day"+data.svgElement);
+          svgElement.getElementById("dayName").text = daysNames[messages[0].day];
+          messages.slice().reverse().forEach((hourWeather, i) => {
+            const hourElement = svgElement.getElementById("hour"+i);
+            hourElement.getElementById("rowWeatherDegrees").text = Math.round(hourWeather.temperature) + "°";
+            if(preferences.clockDisplay === '24h'){
+              hourElement.getElementById("rowWeatherTime").text = hourWeather.hour + ":00";
+            } else {
+              const addString = ([0,1,2,3].includes(i)) ? ' PM' : ' AM';
+              hourElement.getElementById("rowWeatherTime").text = hourWeather.hour%12 + addString;
+            }
+            hourElement.getElementById("rowWeatherIcon").href = "weatherimages/"+hourWeather.icon+".png";
+          });
+        }
+        break;
+      default:
+        break;
+    }
+    
+  }
 }
 
-// Listen for messages from the companion
-messaging.peerSocket.onmessage = function(evt) {
-  const data = evt.data;
-  
-  if (data.enabled === 'true')  {
-    weatherView.style.display = "inline";
-    reloadWeatherButton.style.display = "inline";
-    weatherButton.style.display = "inline";
-    weatherButtonIcon.style.display = "inline";
-    const updateMinutes = data.updateEveryMinutes ? data.updateEveryMinutes : 30;  
-    if (data.temperature) {
-      const el = data.weatherElements[0];
-      cityname.text = data.cityName;
-      img.href = "weatherimages/"+el.icon+".png";
-      degrees.text = Math.round(data.temperature) + "°";
-      weatherButtonIcon.style.display = "none";
-  
-      // Fetch the weather every 30 minutes
-      if(weatherInterval !== null) {
-        clearInterval(weatherInterval);
+const setHeartListener = () => {
+  const heartRateText = document.getElementById("heartratetext");
+  if (HeartRateSensor) {
+    hrm = new HeartRateSensor();
+    hrm.addEventListener("reading", () => {
+      heartRateText.text = hrm.heartRate;
+    });
+    hrm.start();
+  }
+
+  if (BodyPresenceSensor) {
+    bodyPresence = new BodyPresenceSensor();
+    bodyPresence.addEventListener("reading", () => {
+      if (!bodyPresence.present) {
+        heartRateText.text = "--";
+        hrm.stop();
+      } else {
+        hrm.start();
       }
-      weatherInterval = setInterval(fetchWeather, Number(updateMinutes) * 1000 * 60);
-    }
-    if (data.error !== null) {
-      displayToast(data.error);
-    }
-  } else {
-    weatherView.style.display = "none";
-    reloadWeatherButton.style.display = "none";
-    weatherButton.style.display = "none";
-    weatherButtonIcon.style.display = "none";
-    cityname.text = '';
-    img.href = '';
-    degrees.text = '';
+    });
+    bodyPresence.start();
   }
 }
 
-// Listen for the onerror event
-messaging.peerSocket.onerror = function(err) {
-  console.log("Connection error: " + err.code + " - " + err.message);
-}
-
-const hrm = null;
-
-if (HeartRateSensor) {
-  hrm = new HeartRateSensor();
-  hrm.addEventListener("reading", () => {
-    heartRateText.text = hrm.heartRate;
-  });
-  hrm.start();
-}
-
-const bodyPresence = null;
-
-if (BodyPresenceSensor) {
-  bodyPresence = new BodyPresenceSensor();
-  bodyPresence.addEventListener("reading", () => {
-    if (!bodyPresence.present) {
-      heartRateText.text = "--";
-      hrm.stop();
+const setButtonsListeners = () => {
+  reloadWeatherButton.onclick = () => {
+    fetchTodayWeather();
+  }
+  
+  weatherButton.onclick = () => {
+    if (weatherButtonIcon.style.display === "inline"){
+      fetchTodayWeather();
     } else {
-      hrm.start();
+      fetch5daysWeather();
+      container.value = 1;
     }
-  });
-  bodyPresence.start();
-}
-
-
-//AOD setting
-if (display.aodAvailable) {
-  // tell the system we support AOD
-  display.aodAllowed = true;
-
-  const weatherSection = document.getElementById("weather");
-  const heartRateSection = document.getElementById("heartRate");
-  const stepsSection = document.getElementById("steps");
-  const minutesLayer = document.getElementById("minutesLayer");
-  const dateLayer = document.getElementById("todayDate");
-
-  // respond to display change events
-  display.addEventListener("change", () => {
-    // Is AOD inactive and the display is on?
-    if (!display.aodActive && display.on) {
-      container.value = 0;
-      clock.granularity = "seconds";
-      secHand.style.display = "inline";
-      weatherSection.style.display = "inline";
-      heartRateSection.style.display = "inline";
-      stepsSection.style.display = "inline";
-      minutesLayer.style.display = "inline";
-      dateLayer.style.display = "inline";
-      hrm.start();
-      bodyPresence.start();
-    } else {
-      clock.granularity = "minutes";
-      secHand.style.display = "none";
-      weatherSection.style.display = "none";
-      heartRateSection.style.display = "none";
-      stepsSection.style.display = "none";
-      minutesLayer.style.display = "none";
-      dateLayer.style.display = "none";
-      hrm.stop();
-      bodyPresence.stop();
-    }
-  });
-}
-
-reloadWeatherButton.onclick = (evt) => {
-  fetchWeather();
-}
-
-weatherButton.onclick = (evt) => {
-  if (weatherButtonIcon.style.display === "inline"){
-    fetchWeather();
-  } else {
-    container.value = 1;
+  }
+  
+  goToClockButton.onclick = () => {
+    container.value = 0;
   }
 }
 
-goToClockButton.onclick = (evt) => {
-  container.value = 0;
+const setAllListeners = () => {
+  clock.granularity = "seconds"; // Update the clock every second
+
+  setWeatherListener();
+  setHeartListener();
+  setAodListener();
+  setButtonsListeners();
+
+  clock.ontick = () => hanldeClockTick();
 }
+
+setAllListeners();
--- companion/index.js
+++ companion/index.js
@@ -2,8 +2,7 @@ import * as messaging from "messaging";
 import { settingsStorage } from "settings";
 import { geolocation } from "geolocation";
 
-// Fetch the weather from OpenWeather
-function queryOpenWeather() {
+const queryTodayOpenWeather = () => {
   const API_KEY = JSON.parse(settingsStorage.getItem("weatherApiKey")).name ? JSON.parse(settingsStorage.getItem("weatherApiKey")).name : '';
 
   let cityName = JSON.parse(settingsStorage.getItem("weatherCity")).name ? JSON.parse(settingsStorage.getItem("weatherCity")).name : '';
@@ -11,11 +10,11 @@ function queryOpenWeather() {
   const gpsEnabled = settingsStorage.getItem("gpsEnabled");
 
   let weather = {
+    command: 'todayWeather',
     enabled: weatherEnabled,
-    cityName: cityName,
+    cityName: '',
     temperature: '',
-    weatherElements: [],
-    fetchTime: '',
+    weatherElement: {},
     updateEveryMinutes: null,
     error: null
   };
@@ -27,7 +26,7 @@ function queryOpenWeather() {
       geolocation.getCurrentPosition((position) => {
         ENDPOINT += "&lat="+ position.coords.latitude +"&lon=" + position.coords.longitude;
 
-        fetchWeather(ENDPOINT, API_KEY, weather);
+        fetchTodayWeather(ENDPOINT, API_KEY, weather);
       },
       (error) => {
         weather.error = "Failed to use gps";
@@ -38,55 +37,143 @@ function queryOpenWeather() {
 
     } else {
       ENDPOINT += "&q=" + cityName;
-      fetchWeather(ENDPOINT, API_KEY, weather);
+      fetchTodayWeather(ENDPOINT, API_KEY, weather);
     }
   } else {
     messaging.peerSocket.send(weather);
   }
 }
 
-const fetchWeather = (ENDPOINT, API_KEY, weather) => {
+const fetchTodayWeather = (ENDPOINT, API_KEY, weather) => {
   const updateEvery = JSON.parse(settingsStorage.getItem("updateEvery")).values[0].value;
 
   fetch(ENDPOINT + "&APPID=" + API_KEY)
-  .then(function (response) {
+  .then((response) => {
       response.json()
-      .then(function(data) {                
-        weather.temperature = data["main"]["temp"];
-        weather.weatherElements = data["weather"];
-        weather.fetchTime = data["dt"];
+      .then((data) => {                           
+        weather.temperature = data.main.temp;
+        weather.weatherElement = data.weather[0];
         weather.updateEveryMinutes = updateEvery;
-        if (data["name"]) {
-          weather.cityName = data["name"];
+        if (data.name) {
+          weather.cityName = data.name;
+        } else {
+          weather.cityName = "????";
         }
         // Send the weather data to the device          
         messaging.peerSocket.send(weather);
       });
   })
-  .catch(function (err) {
+  .catch((err) => {
     weather.error = "There is an error. Check your weather API key and Internet connection.";
     messaging.peerSocket.send(weather);
   });
 };
 
-// Listen for messages from the device
-messaging.peerSocket.onmessage = function(evt) {
-  if (evt.data && evt.data.command == "weather") {
-    // The device requested weather data
-    queryOpenWeather();
+const query5daysOpenWeather = () => {
+  const API_KEY = JSON.parse(settingsStorage.getItem("weatherApiKey")).name ? JSON.parse(settingsStorage.getItem("weatherApiKey")).name : '';
+
+  let cityName = JSON.parse(settingsStorage.getItem("weatherCity")).name ? JSON.parse(settingsStorage.getItem("weatherCity")).name : '';
+  const weatherEnabled = settingsStorage.getItem("enableWeather");
+  const gpsEnabled = settingsStorage.getItem("gpsEnabled");
+
+  let message = {
+    command: 'forecastWeather',
+    enabled: weatherEnabled,
+    cityName: '',
+    error: null
+  };
+
+  if (weatherEnabled === 'true' && API_KEY && (cityName || gpsEnabled === 'true')) {
+    let ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast" +
+                    "?units=metric";
+    if (gpsEnabled === 'true') {      
+      geolocation.getCurrentPosition((position) => {
+        ENDPOINT += "&lat="+ position.coords.latitude +"&lon=" + position.coords.longitude;
+
+        fetch5daysWeather(ENDPOINT, API_KEY, message);
+      },
+      (error) => {
+        message.error = "Failed to use gps";
+        messaging.peerSocket.send(message);
+      }, {
+        timeout: 60 * 1000
+      });
+
+    } else {
+      ENDPOINT += "&q=" + cityName;
+      fetch5daysWeather(ENDPOINT, API_KEY, message);
+    }
+  } else {
+    messaging.peerSocket.send(message);
   }
 }
 
-// Listen for the onerror event
-messaging.peerSocket.onerror = function(err) {
-  // Handle any errors
+const fetch5daysWeather = (ENDPOINT, API_KEY, message) => {
+  let weatherDaysMessage = [];
+  fetch(ENDPOINT + "&APPID=" + API_KEY)
+  .then((response) => {
+      response.json()
+      .then((data) => {            
+        if (data.city.name) {
+          message.cityName = data.city.name;
+        } else {
+          message.cityName = "????";
+        }
+        let dayWeather = [];
+        let lastDay = null;
+        data.list.forEach((hourlyWeather, i) => {
+          const date = new Date(hourlyWeather.dt *1000);
+          const day = date.getDay();
+          if (lastDay !== day) {
+            if (i !== 0) {
+              weatherDaysMessage.push(dayWeather);
+            }
+            lastDay = day;
+            dayWeather = [];
+          }          
+
+
+          dayWeather.push({
+            day,
+            hour: date.getHours(),
+            icon: hourlyWeather.weather[0].icon,
+            temperature: hourlyWeather.main.temp
+          });
+        });
+        
+        messaging.peerSocket.send(message);
+        weatherDaysMessage.forEach((weatherDayMessage, i) => {
+          messaging.peerSocket.send({
+            svgElement: (i+1).toString(),
+            command: 'forecastWeather',
+            weatherDayMessage
+          });
+        });
+      });
+  })
+  .catch((err) => {
+    message.error = "There is an error. Check your weather API key and Internet connection.";
+    messaging.peerSocket.send(message);
+  });
+};
+
+messaging.peerSocket.onmessage = (evt) => {
+  if (evt.data && evt.data.command == "todayWeather") {
+    queryTodayOpenWeather();
+  }
+  if (evt.data && evt.data.command == "forecastWeather") {
+    query5daysOpenWeather();
+  }
+}
+
+messaging.peerSocket.onerror = (err) => {
   console.log("Connection error: " + err.code + " - " + err.message);
 }
 
-settingsStorage.onchange = function(evt) {
+settingsStorage.onchange = (evt) => {
   switch (evt.key) {
     default:
-      queryOpenWeather();
+      queryTodayOpenWeather();
       break;
   }
 }
